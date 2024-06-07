8c59e3a0686bc6b6527c13136b0d6311d103de25
ashechek <ashechek3@gmail.com>
2024-06-07 20:39:54 +0300

    1

---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+  for (let number of numbers) {
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
+  }
+  return filteredNumbers;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
